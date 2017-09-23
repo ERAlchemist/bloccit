b2663dbeef59963662642bd9ea88ea205a114f7d
Eric Albertus <eralchemist@gmail.com>
2017-09-22 21:57:04 -0700

    assignment-12 complete

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -14,4 +14,11 @@
        expect(response).to render_template("about")
      end
    end  
+
+	describe "GET faq" do
+		it "renders the faq template" do
+			get :faq
+			expect(response).to render_template("faq")
+		end
+	end
 end
