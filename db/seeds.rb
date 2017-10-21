388dd23ae3ab9ae0b5440ac64f6170f49fc3d3e4
Eric Albertus <eralchemist@gmail.com>
2017-10-20 23:05:48 -0700

    checkpoint 29 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 require 'random_data'
 
 # Create Users
- 5.times do
-   User.create!(
-   name:     RandomData.random_name,
-   email:    RandomData.random_email,
-   password: RandomData.random_sentence
-   )
- end
- users = User.all
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
 
 15.times do
   Topic.create!(
@@ -18,26 +18,28 @@ require 'random_data'
 end
 topics = Topic.all
 
- 50.times do
+50.times do
+post = Post.create!(
+  user:   users.sample,
+  topic:  topics.sample,
+  title:  RandomData.random_sentence,
+  body:   RandomData.random_paragraph
+)
 
-   Post.create!(
-		user:  users.sample,
-    topic:  topics.sample,
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+posts = Post.all
  
  # Create Comments
 
- 100.times do
-   Comment.create!(
-    user: users.sample,
-    post: posts.sample,
-    body: RandomData.random_paragraph
-   )
- end
+100.times do
+  Comment.create!(
+  user: users.sample,
+  post: posts.sample,
+  body: RandomData.random_paragraph
+  )
+end
 
 100.times do
 	Question.create!(
@@ -47,27 +49,27 @@ topics = Topic.all
 	)
 end
 
-	# Create an admin user
-  admin = User.create!(
-    name:     'Admin User',
-    email:    'admin@example.com',
-    password: 'helloworld',
-    role:     'admin'
-  )
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
   
-  # Create a member
-  member = User.create!(
-    name:     'Member User',
-    email:    'member@example.com',
-    password: 'helloworld'
-  )
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
  
- puts "Seed finished"
- puts "#{User.count} users created"
- puts "#{Topic.count} topics created"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
- puts "#{Question.count} questions created"
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
 
- unique_post = Post.find_or_create_by!(title: "The Sky", body: "Why is the sky blue?")
- Comment.find_or_create_by!(post: unique_post, body: "The sky is an ocean.")
+unique_post = Post.find_or_create_by!(title: "The Sky", body: "Why is the sky blue?")
+Comment.find_or_create_by!(post: unique_post, body: "The sky is an ocean.")
